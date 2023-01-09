6d8ea272cdbd3c3218bc267277a2df1a681ea1b9
akbota <a.nasvetova91@gmail.com>
2023-01-09 12:54:20 -0700

    added new package

---
+++ src/bota/Bota.java
@@ -0,0 +1,13 @@
+package bota;
+
+public class Bota {
+
+    public static void main(String[] args) {
+
+        System.out.println("Bota");
+
+        // this is first project creating by Bota
+    }
+
+
+}
